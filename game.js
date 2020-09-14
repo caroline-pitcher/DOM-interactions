aa7dec8442102aa06a85d9ed4c61c6c60b9742e9
Caz <cjpitcher@gmail.com>
2020-09-14 13:23:36 +1200

    Adding more code to get function to work

--- game.js
+++ game.js
@@ -40,9 +40,9 @@ function updateCounts () {
     invisible: 0
   }
   
-  document.getElementsByClassName('blue').length = displayTotals()
-  document.getElementsByClassName('green').length = displayTotals()
-  document.getElementsByClassName('invisible').length = displayTotals()// WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  document.getElementsByClassName('blue').length = totals('blue')[0]
+  document.getElementsByClassName('green').length = totals('green')[0]
+  document.getElementsByClassName('invisible').length = totals('invisible')[0]// WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
